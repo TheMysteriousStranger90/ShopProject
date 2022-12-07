27de015732770a28d8c7417cd1536e461fdc9956
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2022-12-07 12:20:06 +0200

    add context class...

---
+++ DAL/Context/ShopProjectContext.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Context
+{
+    public class ShopProjectContext : IdentityDbContext<User>
+    {
+        public ShopProjectContext(){}
+
+        public ShopProjectContext(DbContextOptions<ShopProjectContext> options) : base(options)
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=ShopProjectDb;MultipleActiveResultSets=true");
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+
+        }
+    }
+}
